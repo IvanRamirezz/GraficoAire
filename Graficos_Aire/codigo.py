d941c82ac716ab33daba20aa1032acf584d450bd
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2023-08-12 08:38:07 -0600

    Add files via upload

---
+++ Graficos_Aire/codigo.py
@@ -0,0 +1,13 @@
+# instalar y importar las librerias
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+# Lee el archivo CSV especificando el formato de fecha
+aq = pd.read_csv("air_quality_no8.csv", index_col=0, parse_dates=True, dayfirst=True)
+# Ver los 5 primeros datos
+aq.head()
+# Función plot -  imprimir datos de datos
+aq.plot()
+# plotear una sola estación
+aq['station_3'].plot()
